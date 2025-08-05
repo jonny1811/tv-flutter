b5a7d6f4d8e7ed1b3844f941b380c1edc8f48392
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-08-05 11:33:53 -0300

    feat: add to handle failures and erros

--- lib/app/data/services/remote/trending_api.dart
+++ lib/app/data/services/remote/trending_api.dart
@@ -1,14 +1,19 @@
+import '../../../domain/either/either.dart';
 import '../../../domain/enums.dart';
+import '../../../domain/failures/http_request/http_request_failure.dart';
 import '../../../domain/models/media/media.dart';
 import '../../../domain/typedefs.dart';
 import '../../http/http.dart';
+import '../utils/handle_failure.dart';
 
 class TrendingAPI {
   final Http _http;
 
   TrendingAPI(this._http);
 
-  getMoviesAndSeries(TimeWindow timeWindow) async {
+  Future<Either<HttpRequestFailure, List<Media>>> getMoviesAndSeries(
+    TimeWindow timeWindow,
+  ) async {
     final result = await _http.request(
       '/trending/all/${timeWindow.name}',
       onSuccess: (json) {
@@ -19,6 +24,10 @@ class TrendingAPI {
             .map((e) => Media.fromJson(e))
             .toList();
       },
+    );
+    return result.when(
+      left: handleHttpFailure, 
+      right: (list) => Either.right(list),
     );
   }
 }
