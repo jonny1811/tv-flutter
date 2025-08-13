cc5c701d0862f22f92f4c109e39c986ced206274
Jonny David Lezcano <jonnyda1811@gmail.com>
2025-08-12 21:17:28 -0300

    feat: separate in other widget movie content header and add overview text

--- lib/app/presentation/modules/movie/views/widgets/movie_content.dart
+++ lib/app/presentation/modules/movie/views/widgets/movie_content.dart
@@ -1,8 +1,7 @@
-import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../global/utils/get_image_url.dart';
 import '../../controller/state/movie_state.dart';
+import 'movie_header.dart';
 
 class MovieContent extends StatelessWidget {
   const MovieContent({super.key, required this.state});
@@ -12,82 +11,15 @@ class MovieContent extends StatelessWidget {
   Widget build(BuildContext context) {
     final movie = state.movie;
 
-    return  SingleChildScrollView(
+    return SingleChildScrollView(
       child: Column(
         children: [
-          Stack(
-            children: [
-              AspectRatio(
-                aspectRatio: 16 / 13,
-                child: ExtendedImage.network(
-                  getImageUrl(
-                    movie.backdropPath,
-                    imageQuality: ImageQuality.original,
-                  ),
-                  width: double.infinity,
-                  height: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Positioned(
-                left: 0,
-                bottom: 0,
-                right: 0,
-                child: Container(
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Colors.black54,
-                        Colors.black,
-                      ],
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(15.0).copyWith(
-                    top: 25.0,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        movie.title,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      Wrap(
-                        spacing: 10.0,
-                        children: movie.genres
-                          .map(
-                            (e) => Container(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 5.0,
-                              ),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(4),
-                                border: Border.all(
-                                  color: Colors.white,
-                                ),
-                              ),
-                              child: Text(
-                                e.name,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                ),
-                              ),
-                            ),
-                          )
-                          .toList(),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
+          MovieHeader(movie: movie),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(
+              movie.overview,
+            ),
           ),
         ],
       ),
